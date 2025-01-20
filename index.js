9777d14ca749cbb07b9bc7970f95076a54b24a4a
Duc Hoang <hpmduc3010@gmail.com>
2025-01-20 23:38:22 +0700

    Add cron job

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const { CronJob } = require('cron');
+const { DateTime } = require('luxon');
+const shell = require('shelljs');
+
+const job = new CronJob(
+    '0 */5 * * * *', // Run every 5 minutes
+    () => {
+        console.log(DateTime.local(), '=> Running...');
+        shell.exec(`node ./js/fetch.js -p "ignore" -s "BTC" -i "1m" -f 1 -D`);
+        shell.exec(`python ./py/processor.py ./ignore`);
+        shell.exec(`node ./js/robot.js`);
+    },
+    null,
+    true,
+    'Asia/Saigon',
+    null,
+    true // Run handler on init cron job
+);
